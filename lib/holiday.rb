6a5ff20fdb3ba596b90c646c218a0792fcff16b4
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 22:55:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash.each do |season_name, holidays|
     if(season_name.to_s == season)
-      holiday_hash[season_name] => {holiday_name: supply_array}
+      holiday_hash[season_name] => { holiday_name: "#{supply_array}"}
     end
   end
   holiday_hash
