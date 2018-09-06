32e716c5e0893ab54dd5d276f153124d9985e25f
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 23:36:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
+  
 end
 
 add_new_holiday_with_supplies(holiday_hash, "fall", "halloween", ["chocolate", "candy"])
