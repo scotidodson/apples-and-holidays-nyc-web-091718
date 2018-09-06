57af9abc7ab37d147ba6abce78e8d75d27b0370c
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 23:57:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,8 +96,12 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_names = []
-  holiday_names = holiday_hash.values
-
+  holiday_names.each do |season, holiday_data|
+    holiday_data.each do |holiday, supplies|
+      holiday_names << holiday.capitalize
+    end
+  end
+  holiday_names
 end
 
 
