7645f5eb983499f67c877041fae683b0bb23386a
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 22:51:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
+holiday_hash = {
+     :winter => {
+       :christmas => ["Lights", "Wreath"],
+       :new_years => ["Party Hats"]
+     },
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]
+     },
+     :fall => {
+       :thanksgiving => ["Turkey"]
+     },
+     :spring => {
+       :memorial_day => ["BBQ"]
+     }
+   }
+  
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
@@ -18,12 +34,16 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter].each do |holiday, items|
+    items << supply
+  end
 
 end
 
@@ -31,14 +51,25 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring].each do |holiday, items|
+    if !items.include?(supply)
+      items << supply
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash.each do |season_name, holidays|
+    if(season_name.to_s == season)
+      puts season_name, holidays
+      holidays << {holiday_name => supply_array}
+    end
+  end
+  holiday_hash
 end
+
+add_new_holiday_with_supplies(holiday_hash, "fall", "holiday_name", "supply_array")
+
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
@@ -61,7 +92,6 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
 
 
 
