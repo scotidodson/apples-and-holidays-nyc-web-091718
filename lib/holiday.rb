2c44560437653ce208daa30134d1395658203645
Scoti Dodson <scotidodson@gmail.com>
2018-09-07 00:01:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,11 +96,11 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_names = []
-  holiday_hash[:summer].each do |holiday, supplies|
+  holiday_hash.each do |season, holiday_data|
+    holiday_data.each do |holiday, supplies|
+      if supplies.include?("BBQ")
       holiday_names << holiday
-  end
-  holiday_hash[:spring].each do |holiday, supplies|
-      holiday_names << holiday
+    end
   end
   holiday_names
 end
